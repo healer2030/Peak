68bbf5c7ce4fc2d78cd3bdc18ee909e81e9503fd
Somtochukwu Nwagbata <healer2030@gmail.com>
2019-12-02 13:51:00 -0500

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,42 +15,7 @@ $(document).ready(function () {
     var pCount;//= document.getElementById("f-travellers");
     //console.log(document.getElementById("#f-date").selectedDates[0]);
     // on flightType change configuration
-    $("input[type=radio][name=flightType]").change(function () {
-        if (this.value == "round-trip") {
-            date = $('#f-date').datepicker({
-                language: 'en',
-                minDate: new Date(),
-                range: true,
-                multipleDatesSeparator: " ⁠— "
-            })
-            //change date element attributes and show datepicker
-            $("#f-date").attr("disabled", false);
-            $("#f-date").attr("placeholder", "Depart ⁠— Return");
-            $("#f-date").val('');
-            
-            date.show();
-        }
-        else {
-            date = $('#f-date').datepicker({
-                language: 'en',
-                minDate: new Date(),
-                range: false
-            })
-            //change date element attributes and show datepicker
-            $("#f-date").attr("disabled", false);
-            $("#f-date").attr("placeholder", "Departure date");
-            $("#f-date").val('');
-           
-            date.show();
-        }
-    })
-    date = $('#s-date').datepicker({
-        language: 'en',
-        minDate: new Date(),
-        range: false
-    })
-    date.show();
-    date.blur();
+
 
     $('#f-to').selectize({sortField: 'text'});
     $('#f-from').selectize({sortField: 'text'});
